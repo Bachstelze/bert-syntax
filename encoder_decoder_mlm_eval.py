f0a27af6aaa174ca086ff0890d72d0b0c6b6ac91
Wagtail <Bachstelze@users.noreply.github.com>
2025-04-06 11:13:00 +0200

    Update encoder_decoder_mlm_eval.py

--- encoder_decoder_mlm_eval.py
+++ encoder_decoder_mlm_eval.py
@@ -1,4 +1,4 @@
-from transformers import AutoConfig, AutoModelForMaskedLM, AutoTokenizer, AutoModelForCausalLM, BertTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoConfig, AutoModelForMaskedLM, AutoTokenizer, AutoModelForCausalLM, BertTokenizer, AutoModelForSeq2SeqLM, pipeline
 import torch
 import sys
 import csv
